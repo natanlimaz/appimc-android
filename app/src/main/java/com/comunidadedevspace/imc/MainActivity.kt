7a142d917903fb07465699850daa2545bf4fdcc8
natanlimaz <natanlimadev@gmail.com>
2024-11-24 20:48:42 -0300

    Mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +18,20 @@ class MainActivity : AppCompatActivity() {
         val edtButton = findViewById<Button>(R.id.btn_calcular);
 
         edtButton.setOnClickListener {
-            val peso: Float = edtWeight.text.toString().toFloat();
-            val altura: Float = edtHeight.text.toString().toFloat();
+            val pesoStr: String = edtWeight.text.toString();
+            val alturaStr: String = edtHeight.text.toString();
 
-            val imc = peso / (altura*altura);
+            if(pesoStr.isNotEmpty() && alturaStr.isNotEmpty()) {
+                val peso: Float = pesoStr.toFloat();
+                val altura: Float = alturaStr.toFloat();
 
+                val imc = peso / (altura*altura);
 
-            println("CLIQUE -> " + imc);
+            }
+            else {
+                Snackbar.make(edtWeight, "Preencha todos os campos!", Snackbar.LENGTH_SHORT).show();
+            }
+
         }
 
     }
