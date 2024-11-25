6a984eaee56bbd4439733f3e3f0d8f1af6dcfba5
natanlimaz <natanlimadev@gmail.com>
2024-11-24 21:40:31 -0300

    Criação da tela de resultado e mostrando resultado e classificação na tela

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -26,6 +27,10 @@ class MainActivity : AppCompatActivity() {
                 val altura: Float = alturaStr.toFloat();
 
                 val imc = peso / (altura*altura);
+
+                val intent = Intent(this, ResultActivity::class.java);
+                intent.putExtra(KEY_RESULT_IMC, imc)
+                startActivity(intent);
 
             }
             else {
