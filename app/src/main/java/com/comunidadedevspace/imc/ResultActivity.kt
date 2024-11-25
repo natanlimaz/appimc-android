29e89cb3e88cf29185e0bfe5e641538001911440
natanlimaz <natanlimadev@gmail.com>
2024-11-24 23:28:01 -0300

    Melhorando experiência do usuário

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -21,28 +22,41 @@ class ResultActivity : AppCompatActivity() {
             insets
         }
 
+        window.apply {
+            statusBarColor = ContextCompat.getColor(this@ResultActivity, R.color.primary_900)
+        }
+
         val result = intent.getFloatExtra(KEY_RESULT_IMC, 0f);
         val tvResult = findViewById<TextView>(R.id.tv_result);
         val tvClassificacao = findViewById<TextView>(R.id.tv_classificacao);
 
         tvResult.text = result.toString();
 
-        val classificacao: String = if(result < 18.5) {
-           "MAGREZA";
+        val classificacao: String;
+        val color: Int;
+
+        if(result < 18.5) {
+            classificacao = "MAGREZA";
+            color = R.color.red;
         }
         else if(result < 25) {
-            "NORMAL";
+            classificacao = "NORMAL";
+            color = R.color.green;
         }
         else if(result < 30) {
-           "SOBREPESO";
+            classificacao = "SOBREPESO";
+            color = R.color.yellow;
         }
         else if(result < 40) {
-           "OBESIDADE";
+            classificacao = "OBESIDADE";
+            color = R.color.red;
         }
         else {
-           "OBESIDADE GRAVE"
+            classificacao = "OBESIDADE GRAVE"
+            color = R.color.red;
         }
 
         tvClassificacao.text = classificacao;
+        tvClassificacao.setTextColor(ContextCompat.getColor(this, color));
     }
 }
